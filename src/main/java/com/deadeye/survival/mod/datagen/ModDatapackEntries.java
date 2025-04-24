4257ae9c59b90c3e28633cd670acbd39b480c493
David Cirenese <davidcirenese@yahoo.com>
2025-04-23 22:41:59 -0400

    adding worldgen

---
+++ src/main/java/com/deadeye/survival/mod/datagen/ModDatapackEntries.java
@@ -0,0 +1,26 @@
+package com.deadeye.survival.mod.datagen;
+
+import com.deadeye.survival.mod.DeadeyeMod;
+import com.deadeye.survival.mod.worldgen.ModBiomeModifiers;
+import com.deadeye.survival.mod.worldgen.ModConfiguredFeatures;
+import com.deadeye.survival.mod.worldgen.ModPlacedFeatures;
+import net.minecraft.core.HolderLookup;
+import net.minecraft.core.RegistrySetBuilder;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.PackOutput;
+import net.minecraftforge.common.data.DatapackBuiltinEntriesProvider;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+
+public class ModDatapackEntries extends DatapackBuiltinEntriesProvider {
+    public static final RegistrySetBuilder BUILDER = new RegistrySetBuilder()
+            .add(Registries.CONFIGURED_FEATURE, ModConfiguredFeatures::bootstrap)
+            .add(Registries.PLACED_FEATURE, ModPlacedFeatures::bootstrap)
+            .add(ForgeRegistries.Keys.BIOME_MODIFIERS, ModBiomeModifiers::bootstrap);
+
+    public ModDatapackEntries(PackOutput output, CompletableFuture<HolderLookup.Provider> registries) {
+        super(output, registries, BUILDER, Set.of(DeadeyeMod.MOD_ID));
+    }
+}
